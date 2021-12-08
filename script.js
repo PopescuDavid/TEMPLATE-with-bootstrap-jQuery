fb3152c21b27ddbd567f697b4f1f58d3dbe33ca2
davidpopescu <davidpopescu2002@gmail.com>
2021-12-08 19:55:21 +0200

    final

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $(function(){
     $("header").height($(window).height());
-    $("#toggler-button").click(function(){
-        $(".nav-item").addClass("toggle-background");
+    $("#toggler-button").on("click", function(){
+        $(".btn1 i").toggleClass("fa-bars fa-window-close");
     });
 });
 
-$("#toggler-button").on("click", function(){
-    $("button i").toggleClass("fa-bars fa-window-close");
+$(function(){
+    $("#pages-button").on("click", function(){
+        $(".btn2 i").toggleClass("fa-plus-square fa-window-close");
+    });
 });
+
+
+
+
