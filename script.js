3e7abdb75caf7269774b26598a1c9e405700ef43
davidpopescu <davidpopescu2002@gmail.com>
2021-12-08 11:30:16 +0200

    final

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(function(){
-        $("header").height()=$(window).height();
+    $("header").height($(window).height());
+    $("#toggler-button").click(function(){
+        $(".nav-item").addClass("toggle-background");
+    });
+});
+
+$("#toggler-button").on("click", function(){
+    $("button i").toggleClass("fa-bars fa-window-close");
 });
